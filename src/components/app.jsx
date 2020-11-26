a0aac422e23cef6f67153ecc47d1089a92634d5a
Md Tazri <mdtazri@gmail.com>
2020-11-26 09:38:06 +0600

    I learn some react feature.First I learn React fragment.What is react fragment and how to use it.It easy is crazy easy.It for multiplay sibling return from render method in class.React fragment look like (\<React.Fragment\>(some JSX code)\<React.Fragment/\>).It sort hand is (\<\>(some JSX code)\</\>).Sort hand to easy to use and understand.Sort hand for react fragment not work react 6 lower version.It work upper version then 6.Then We learn how use costum component in return of rander method.It to easy.It use like html self tag.Soppose (MyComponent) is costum component which return some JSX code.So it use in return of render method like (\<MyComponent/\>).It look like html self tag.One more think if we use costum component in return of rander method so we will write first letter of costum component with captial letter.If not do it so it not work.If we use JavaScript syntax in JSX code.so We will use {}.JavaScript Code also inside the {} both curly bracket.It only work for oneline JavaScript code.It not work for multiply line.Like if else stetment.Only one line statement

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,22 +1,79 @@
 import React, { Component } from 'react'
 
+//Costum component in ract 
+let MyComponent = ()=><h1>Hello World,My name is Md Tazri</h1>
+
 class App extends Component{
     
     render(){
 
+        //name variable
+        let name = 'Md Tazri'
+
+        //attribute for h1 tag
+        let attribute = {
+            title : 'I am Md Tazri',
+            id : 'MdTazri'
+        }
+        //end attribute for h1 tag
+
+        //bio for me
+        let bio = (
+            <div>
+                <h3>My name is Md Tazri</h3>
+                <p>One day i will be programmer and web developer</p>
+            </div>
+        )
+
+        //React Write Code by JavaScript Core
         // return React.createElement('div',{
         //     className:'App'
         // },[
         //     React.createElement('h1',null,'What is JSX?'),
         //     React.createElement('p',null,'JSX is some javaScript code.It mean javaScript extention.')
         // ])
+        //End React Write Code by JavaScript Core
 
+        //React Write Code by JSX Sugar Code and Thats is Crazy Easy.
+        // return(
+        //     <div className="App">
+        //         <h1>What is JSX?</h1>
+        //         <p>JSX is some javaScript code.It mean javaScript extention.</p>
+        //     </div>
+        // )
+        //End React Write Code by JSX sugar code and thats is crazy easy.
+
+        //code by react fragment
+        // return(
+        //     <React.Fragment>
+        //         <h1>What is JSX?</h1>
+        //         <p>JSX is Some JavaScript code.It mean JavaScript extention.</p>
+        //     </React.Fragment>
+        // )
+        // //End code by react fragment
+
+        // //code by react fragment sort hand 
+        // return(
+        //     <>
+        //         <h1>What is JSX?</h1>
+        //         <p>JSX is some JavaScript code.It mean JavaScript extention.</p>
+        //     </>
+        // )
+        // //end by react fragment sort hand
+
+        //react JSX simple sugar
         return(
-            <div className="App">
+            <div className="App" {...attribute}>
                 <h1>What is JSX?</h1>
-                <p>JSX is some javaScript code.It mean javaScript extention.</p>
+                <p>JSX is some JavaScript code.It mean JavaScript extention.</p>
+                <MyComponent/>
+                <h2>My name is {name}</h2>
+                <hr/>
+                {bio}
             </div>
         )
+
+
     }
 }
 
@@ -35,12 +92,12 @@ export default App;
 
 
 //Element Property and value in React js
-
-const element = {
-    type : 'div',
-    props : {
-        className : 'Test',
-        title : 'testMe'
-    },
-    children: ['p','h1','div'] | 'text' | null
-}
+// const element = {
+//     type : 'div',
+//     props : {
+//         className : 'Test',
+//         title : 'testMe'
+//     },
+//     children: ['p','h1','div'] | 'text' | null
+// }
+//End Element Property and Value in React js
