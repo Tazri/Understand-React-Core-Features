409b12a27627d65d50eaf657c74863d180cc79ad
Md Tazri <mdtazri@gmail.com>
2020-11-28 00:38:11 +0600

    Now I know why use state and how to use state.One more think how to update sate.Why I dont update sate directly.This is awosome.If I want update state so I use setSate function.If i update in class components so I will use this keyword for use setState function.It first argument should be object.If it first argument function so it will return object.setState secound argument is function.This function for if I work with update state value when it update.This function for setState  is asycn.So I will use secound argument if I want extra work with update value when it update.It realy awosome and crazy easy.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,13 +2,26 @@ import React from 'react'
 
 
 class App extends React.Component{
+    //normal propert 
     // constructor(props){
     //     super(props);
     //     this.count = 0;
     // }
-
+    
     count = 0;
+    //end normal property
 
+    //state
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         count : 0
+    //     }
+    // }
+     
+    state = {
+        count : 0
+    }
     render(){
         return(
             <div>
@@ -21,6 +34,23 @@ class App extends React.Component{
                 }}>Add One</button>
                 <h1>Why use State?</h1>
                 <p>Now we see that above.If we use normal property in JSX code.If we update this property in future so property will be update but JSX code where use out property is not update.This why We use State in React.</p>
+                <h1>Try to same think again by react state</h1>
+                <h2>This is Count and this value is = {this.state.count}</h2>
+                <button onClick={()=>{
+                    // this.setState({count : this.state.count+1});
+                    //it not work for update state
+                    // this.state.count = this.state.count + 1;
+                     this.setState(prev =>{
+                        return {
+                            count : this.state.count +1
+                        }
+                     },()=>{
+                        console.log('this is state count clicked....',this.state.count);
+                     })
+
+                    //Below this code not update with state because state is asycn.
+                    // console.log('this is state count clicked....',this.state.count);
+                }}>Add One</button>
             </div>
         )
     }
